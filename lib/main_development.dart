f49ba653c5ea9ae85518d157b7440232b3065595
Enzo Lizama <lizama.enzo@gmail.com>
2024-09-09 04:47:58 -0500

    chore: update entry point

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,11 +1,21 @@
 import 'package:cinema_api_client/cinema_api_client.dart';
 import 'package:cinema_app/app/app.dart';
 import 'package:cinema_app/bootstrap.dart';
+import 'package:dio/dio.dart';
 import 'package:movie_repository/movie_repository.dart';
 
 void main() {
   bootstrap(() async {
-    final apiClient = CinemaApiClient('https://api.themoviedb.org/3');
+    const apiKey = String.fromEnvironment('API_KEY');
+    const baseUrl = 'https://api.themoviedb.org/3';
+    final httpClient = Dio(BaseOptions(baseUrl: baseUrl))
+      ..interceptors.add(LogInterceptor(requestBody: true));
+
+    final apiClient = CinemaApiClient(
+      baseUrl,
+      apiKey: apiKey,
+      httpClient: httpClient,
+    );
     final movieRepository = MovieRepository(apiClient: apiClient);
     return CinemaMovieApp(movieRepository: movieRepository);
   });
