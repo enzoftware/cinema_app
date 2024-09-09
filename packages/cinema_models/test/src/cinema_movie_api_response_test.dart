1b0651904a14f14fa2462ce8138474e4fbeee04c
Enzo Lizama <lizama.enzo@gmail.com>
2024-09-09 04:28:50 -0500

    test: update models for models

--- packages/cinema_models/test/src/cinema_movie_api_response_test.dart
+++ packages/cinema_models/test/src/cinema_movie_api_response_test.dart
@@ -83,6 +83,39 @@ void main() {
       );
     });
 
+    test('formattedReleaseDate', () {
+      final movie = MovieResult.fromJson(
+        const {
+          'adult': false,
+          'backdrop_path': '/6IrZ3C8qSZ8Tbb32s41ReJOXpI0.jpg',
+          'genre_ids': [
+            12,
+            10751,
+            14,
+            35,
+          ],
+          'id': 826510,
+          'original_language': 'en',
+          'original_title': 'Harold and the Purple Crayon',
+          'overview':
+              'Inside of his book, adventurous Harold can make anything.',
+          'popularity': 742.65,
+          'poster_path': '/dEsuQOZwdaFAVL26RjgjwGl9j7m.jpg',
+          'release_date': '2024-07-31',
+          'title': 'Harold and the Purple Crayon',
+          'video': false,
+          'vote_average': 6.799,
+          'vote_count': 92,
+        },
+      );
+      expect(movie.formattedReleaseDate, '2024-07-31');
+    });
+
+    test('toFormattedDate', () {
+      final date = DateTime(2024, 9, 12);
+      expect(date.toFormattedDate(), '2024-09-12');
+    });
+
     test('parsing fromJson', () async {
       final response = CinemaMovieApiResponse.fromJson(json);
       expect(response, isNotNull);
